0bcd2dc132665a709ca9dbf5470f1063b7f64607
Kitty Davies <kittyfdavies@gmail.com>
2018-02-26 23:14:12 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current stated.
 def display_board(board)
-  puts " #{ } | #{ } | #{ } /n-----------/n #{ } | #{ } | #{ } /n-----------/n #{ } | #{ } | #{ } "
+  puts " #{0} | #{1} | #{2} /n-----------/n #{3} | #{4} | #{5} /n-----------/n #{6} | #{7} | #{8} "
 end
 
-board = ['X',' ',' ',' ',' ',' ',' ',' ',' ']
+board = ['X','X','X',' ',' ',' ',' ',' ',' ']
